b4662baeb01e79376cd87e797dd4dee5ba89e75a
Martin Konopka <martkono@gmail.com>
2019-12-04 16:58:41 +0100

    removed debugging code

--- day-04/part-2.ps1
+++ day-04/part-2.ps1
@@ -3,8 +3,8 @@
         $orig = $_.ToString().ToCharArray()
         $shifted = ([int]([Math]::Floor($_ / 10))).ToString().ToCharArray()
         1..($orig.Length - 1) | % { $orig[$_] = ([int]::Parse($orig[$_]) - [int]::Parse($shifted[$_ - 1])).ToString()[0] }
-        @{ "orig" = $_ ; "proc" = [String]::new($orig) }
+        [String]::new($orig)
     } `
-| Where-Object -FilterScript { (-not $_["proc"].Contains("-")) -and ($_["proc"] -match "([^0]0[^0])|([^0]0$)") } `
+| Where-Object -FilterScript { (-not $_.Contains("-")) -and ($_ -match "([^0]0[^0])|([^0]0$)") } `
 | Measure-Object `
 | Select-Object -Expand Count
